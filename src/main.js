a3be45fe30072d1828c7f6b34286c08753f634de
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-04-16 12:52:20 +0200

    Create function to fetch work experience

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Fetch work experience data 
+async function getWorkExperience() {
+    let url = "http://localhost:3000/work_experience";
+    
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const data = await response.json();
+        console.log("Work experience", data);
+    } catch (error) {
+        console.error("Error fetching data", error)
+    }
+}
+
+getWorkExperience();
